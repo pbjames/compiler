c7586d311b36fa380ed0076fa9f350235380ddca
James <jameskowal10@gmail.com>
2025-02-27 21:28:28 +0000

    Write basic tests and syntax

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -1,6 +1,23 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+  Statement (..),
+  Expr (..),
+  BinaryOperator (..),
+  maxArgs,
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type Identifier = String
+data BinaryOperator = Plus | Minus | Times | Divide
+data Expr
+  = Id Identifier
+  | Number Int
+  | Op BinaryOperator Expr Expr
+  | Eseq Statement Expr
+data Statement
+  = Compound Statement Statement
+  | Assign Identifier Expr
+  | Print [Expr]
+
+maxArgs :: Statement -> Int
+maxArgs (Compound s t) = max (maxArgs s) (maxArgs t)
+maxArgs (Print exprs) = length exprs
+maxArgs (Assign _ _) = 0
--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -1,2 +1,21 @@
+import Lib (BinaryOperator (..), Expr (..), Statement (..), maxArgs)
+import Test.HUnit (Test (..), assertEqual, runTestTTAndExit)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = runTestTTAndExit tests
+
+exampleForMaxArgs :: Statement
+exampleForMaxArgs =
+  Compound
+    (Assign "a" (Op Plus (Number 3) (Number 5)))
+    (Print [Number 3, Id "a"])
+
+tests :: Test
+tests =
+  TestList
+    [ TestCase $
+        assertEqual
+          "maxArgs-1"
+          2
+          (maxArgs exampleForMaxArgs)
+    ]
